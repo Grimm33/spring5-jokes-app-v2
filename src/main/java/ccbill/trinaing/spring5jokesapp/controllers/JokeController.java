bf2f23619b849abf454386c850b125f60f08a81a
Grimm33 <yandrick.alpha.grima@gmail.com>
2021-04-05 10:16:17 +0200

    Add Joke controller. Closes #4

---
+++ src/main/java/ccbill/trinaing/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package ccbill.trinaing.spring5jokesapp.controllers;
+
+import ccbill.trinaing.spring5jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    //can omit @Autowired since we only have 1 constructor
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
